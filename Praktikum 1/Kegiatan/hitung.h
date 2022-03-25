b1259a8676ef4c211f45ee4ca956b682207eff50
Sidikara7 <sidikarahakim04@gmail.com>
2022-03-25 11:21:08 +0700

    Kegiatan 1

---
+++ Praktikum 1/Kegiatan/hitung.h
@@ -0,0 +1,16 @@
+#ifndef HITUNG_H
+#define HITUNG_H
+#include <iostream>
+#include <conio.h>
+using namespace std;
+class Hitung{
+	friend ostream& operator<<(ostream&, const Hitung&);
+	friend istream& operator>>(istream&, Hitung&);
+public:
+	Hitung();
+	void hitung_jumlahnya(){ jumlah = (a + b + c); }
+private:
+	int a,b,c;
+	int jumlah;
+};
+#endif
